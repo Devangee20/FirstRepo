57330a7b85944c7d863376f3f469eb560f7a23d6
Devangee Jain <devangee20@gmail.com>
2026-05-11 14:07:20 +0530

    Created a new file

---
+++ chini.java
@@ -0,0 +1,10 @@
+package FirstRepo;
+
+public class chini {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 60;
+        int c = a + b;
+        System.out.println("Sum is " + c);
+    }
+}
